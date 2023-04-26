3f1038b93cbf346f6bd2c6cfe36c3d3740d6af3a
souvik ghosh <souvik_g10@yahoo.com>
2023-04-26 23:39:51 +0200

    add docs for action server plugins

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -218,6 +218,7 @@ module.exports = {
                                 },
                             ],
                         },
+                        'action-server/add-sanic-extensions'
                     ],
                 },
             ],
